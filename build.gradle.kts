26d0c286611f332222ed195da9e257f610dd4dbe
booky10 <boooky10@gmail.com>
2022-08-20 13:22:08 +0200

    Added gradle buildscript and empty main class

---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("application")
+    id("java-library")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "dev.booky"
+version = "1.0.0"
+val bootClass = "$group.nbtfmt.${name}Main"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("net.kyori:adventure-nbt:4.11.0")
+}
+
+java {
+    withSourcesJar()
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifactId = project.name.toLowerCase()
+        from(components["java"])
+    }
+}
+
+application {
+    mainClass.set(bootClass)
+}
+
+tasks {
+    withType<JavaExec> {
+        workingDir = projectDir.resolve("run")
+        workingDir.mkdirs()
+    }
+
+    jar {
+        manifest.attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version,
+            "Implementation-Vendor" to "booky10",
+            "Main-Class" to bootClass
+        )
+    }
+
+    shadowJar {
+        minimize()
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ src/main/java/dev/booky/nbtfmt/NbtFormatterMain.java
@@ -0,0 +1,9 @@
+package dev.booky.nbtfmt;
+// Created by booky10 in NbtFormatter (13:17 20.08.22)
+
+public class NbtFormatterMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
