525a5bcab6f3635e079a7d993ca953ae5ea899e5
booky10 <boooky10@gmail.com>
2023-04-16 18:29:01 +0200

    Clean up web code
    Made textarea responsive and now uses grid layout

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,16 +1,19 @@
-const inputArea = document.getElementById("input");
-const statusInfo = document.getElementById("status");
+const input = document.getElementById("input");
+const status = document.getElementById("status");
+const message = document.getElementById("message");
 
 const copyButton = document.getElementById("copy");
 const reformatButton = document.getElementById("reformat");
 const minifyButton = document.getElementById("minify");
 
 function reformat(indent) {
-    inputArea.disabled = true;
+    input.disabled = true;
+    status.innerText = "Formatting...";
+    message.innerText = "";
+
     copyButton.disabled = true;
     reformatButton.disabled = true;
     minifyButton.disabled = true;
-    statusInfo.innerHTML = "<span>Formatting...</span>";
 
     const req = new XMLHttpRequest();
     req.open("POST", `${location.href}api/v1/format?indent=${indent}`, true);
@@ -19,30 +22,37 @@ function reformat(indent) {
             return;
         }
 
-        inputArea.disabled = false;
+        input.disabled = false;
         copyButton.disabled = false;
         reformatButton.disabled = false;
         minifyButton.disabled = false;
 
+        const totalTime = (req.getResponseHeader("Total-Time") / 1000000).toFixed(2);
+
         if (req.status != 200) {
-            statusInfo.innerHTML = `<span>Error ${req.status} while reformatting: </span><code>${req.responseText}</code>`;
+            status.innerText = `Error ${req.status} while reformatting${totalTime > 0 ? ` (took ${totalTime}ms)` : ""}:`;
+            message.innerText = req.responseText;
             return;
         }
 
-        statusInfo.innerHTML = "";
-        inputArea.value = req.responseText;
+        const parsingTime = (req.getResponseHeader("Parsing-Time") / 1000000).toFixed(2);
+        const formatTime = (req.getResponseHeader("Format-Time") / 1000000).toFixed(2);
+
+        status.innerText = `Reformatting took ${totalTime}ms (${parsingTime}ms parsing, ${formatTime}ms formatting)`;
+        input.value = req.responseText;
     };
 
     req.setRequestHeader("Content-Type", "text/plain");
-    req.send(inputArea.value);
+    req.setRequestHeader("Accept", "text/plain");
+    req.send(input.value);
 }
 
 copyButton.onclick = () => {
-    inputArea.focus();
-    inputArea.select();
+    input.focus();
+    input.select();
 
     if (navigator.clipboard) {
-        navigator.clipboard.writeText(inputArea.value);
+        navigator.clipboard.writeText(input.value);
     } else {
         // fallback
         document.execCommand("copy");
